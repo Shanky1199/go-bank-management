60efd1cb1ae1d194237f118bcf2dde895066fb30
Shashank <shashanks939@gmail.com>
2023-08-13 13:38:13 +0530

    adding all the files to run this

---
+++ gapi/converter.go
@@ -0,0 +1,17 @@
+package gapi
+
+import (
+	db "github.com/Shanky1199/go-bank-management/db/sqlc"
+	"github.com/Shanky1199/go-bank-management/pb"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func convertUser(user db.User) *pb.User {
+	return &pb.User{
+		Username:          user.Username,
+		FullName:          user.FullName,
+		Email:             user.Email,
+		PasswordChangedAt: timestamppb.New(user.PasswordChangedAt),
+		CreatedAt:         timestamppb.New(user.CreatedAt),
+	}
+}
